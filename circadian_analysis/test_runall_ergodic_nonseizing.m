1cbf07c5983ad81cc99b1be8dd03d30102c95e35
David Stanley <davearthur.stanley@gmail.com>
2018-11-02 00:11:13 -0400

    Added code from 14a_def_chronux to for automated calculations 	test_runall_ergodic.m 	test_runall_ergodic_nonseizing.m

---
+++ circadian_analysis/test_runall_ergodic_nonseizing.m
@@ -0,0 +1,79 @@
+
+
+% r01_theta =  ratscript_FFT_thetadelta2_arr (1)
+% r04_theta =  ratscript_FFT_thetadelta2_arr (4)
+% r09_theta =  ratscript_FFT_thetadelta2_arr (9)
+% r10_theta =  ratscript_FFT_thetadelta2_arr (10)
+% r5_theta =  ratscript_FFT_thetadelta2_arr (5)
+% r8_theta =  ratscript_FFT_thetadelta2_arr (8)
+% r11_theta =  ratscript_FFT_thetadelta2_arr (11)
+% r12_theta =  ratscript_FFT_thetadelta2_arr (12)
+% save All4T1_9pm_3am_ratio1p5_theta6_10_delta0_6.mat
+
+
+
+
+extract_circadian_info = 0;
+cosinor_mode = 1;
+extract_band = [1 5; 5 10; 12 25; 25 50; 50 90; 90 140; 140 200 ; 200 500];
+
+
+timerange0 = 1
+r01_theta = ratscript_FFT_thetadelta2_arr (8, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r04_theta = ratscript_FFT_thetadelta2_arr (11, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r09_theta = ratscript_FFT_thetadelta2_arr (5, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r10_theta = ratscript_FFT_thetadelta2_arr (12, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+save All4T1.mat
+
+
+timerange0 = 2;
+r01_theta = ratscript_FFT_thetadelta2_arr (8, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r04_theta = ratscript_FFT_thetadelta2_arr (11, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r09_theta = ratscript_FFT_thetadelta2_arr (5, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r10_theta = ratscript_FFT_thetadelta2_arr (12, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+save All4T2.mat
+
+timerange0 = 3;
+r01_theta = ratscript_FFT_thetadelta2_arr (8, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r04_theta = ratscript_FFT_thetadelta2_arr (11, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r09_theta = ratscript_FFT_thetadelta2_arr (5, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r10_theta = ratscript_FFT_thetadelta2_arr (12, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+save All4T3.mat
+
+timerange0 = 4;
+r01_theta = ratscript_FFT_thetadelta2_arr (8, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r04_theta = ratscript_FFT_thetadelta2_arr (11, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r09_theta = ratscript_FFT_thetadelta2_arr (5, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r10_theta = ratscript_FFT_thetadelta2_arr (12, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+save All4T4.mat
+
+timerange0 = 5;
+r01_theta = ratscript_FFT_thetadelta2_arr (8, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r04_theta = ratscript_FFT_thetadelta2_arr (11, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r09_theta = ratscript_FFT_thetadelta2_arr (5, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r10_theta = ratscript_FFT_thetadelta2_arr (12, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+save All4T5.mat
+
+timerange0 = 6;
+r01_theta = ratscript_FFT_thetadelta2_arr (8, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r04_theta = ratscript_FFT_thetadelta2_arr (11, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r09_theta = ratscript_FFT_thetadelta2_arr (5, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r10_theta = ratscript_FFT_thetadelta2_arr (12, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+save All4T6.mat
+
+timerange0 = 7;
+r01_theta = ratscript_FFT_thetadelta2_arr (8, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r04_theta = ratscript_FFT_thetadelta2_arr (11, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r09_theta = ratscript_FFT_thetadelta2_arr (5, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r10_theta = ratscript_FFT_thetadelta2_arr (12, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+save All4T7.mat
+
+timerange0 = 8;
+r01_theta = ratscript_FFT_thetadelta2_arr (8, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r04_theta = ratscript_FFT_thetadelta2_arr (11, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r09_theta = ratscript_FFT_thetadelta2_arr (5, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+r10_theta = ratscript_FFT_thetadelta2_arr (12, 2,[5 10],[1 5],timerange0, extract_circadian_info,cosinor_mode,extract_band)
+save All4T8.mat
+
+
+
