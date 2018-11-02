dd7a04e563149a02e6d82cf42c5efe7817bbfe75
David Stanley <davearthur.stanley@gmail.com>
2018-11-02 14:39:17 -0400

    Updated matup setup files

--- circadian_analysis/matup_Mac.m
+++ circadian_analysis/matup_Mac.m
@@ -1,7 +1,11 @@
 
 
-
-currdir = pwd;
-cd ('/Users/davestanley/GDrive/from_Dropbox/MATLAB/Evol/utility');
-set_path_evol
-cd (currdir)
+% Setup paths to submodules and supporting functions
+restoredefaultpath
+addpath(genpath(fullfile('..','submodules','lib_MAScPhD_Matlab')));
+addpath(genpath(fullfile('..','submodules','lib_dav')));
+addpath(genpath(fullfile('..','submodules','SigProc-Plott')));
+addpath(genpath(fullfile('..','submodules','chronux')));
+addpath(fullfile('.','cosinor_dav'));
+addpath(fullfile('.','funcs_supporting'));
+addpath(fullfile('.','funcs_supporting_demo'));
