dac7fae4f5d010309972e9665885704cfe37360f
David Stanley <davearthur.stanley@gmail.com>
2018-11-03 19:35:04 -0400

    Modified mount_Raw_ln.sh for storing RawOneK data locally

--- data/mount_Raw_ln.sh
+++ data/mount_Raw_ln.sh
@@ -1,3 +1,5 @@
 
 # Mounts raw data from external drive
-ln -s /Volumes/CALCUTTA/Archive/data_JNeurophys/Raw/EEG_SPK/RawOneK ./Raw_ln/Evol/RawOneK 
+#ln -s /Volumes/CALCUTTA/Archive/data_JNeurophys/Raw/EEG_SPK/RawOneK ./Raw_ln/Evol/RawOneK 
+ln -s /Users/davestanley/from_Calcutta_deleteme/RawOneK  ./Raw_ln/Evol/RawOneK 
+
